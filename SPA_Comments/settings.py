0a86588e6e7c08d0002a2b516b0e7fa62fd7c85f
pit-rella <serezha.sliva@gmail.com>
2024-07-31 17:34:05 +0300

    Add CAPTCHA and update forms and URL configurations
    - Installed and integrated CAPTCHA functionality
    - Edited comments/forms.py to include CAPTCHA field
    - Modified urls.py to accommodate new CAPTCHA setup

--- SPA_Comments/settings.py
+++ SPA_Comments/settings.py
@@ -35,7 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'comments'
+    'comments',
+    'captcha',
 ]
 
 MIDDLEWARE = [
