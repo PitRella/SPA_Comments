6f8f9a64f01eca6136485b9ccf8f85475c1d9d02
pit-rella <serezha.sliva@gmail.com>
2024-08-01 10:55:31 +0300

    Added pagination: updated comment_list.html to display pagination and modified views.py to support pagination functionality.

--- comments/views.py
+++ comments/views.py
@@ -2,19 +2,33 @@ from django.shortcuts import render, redirect
 from .models import Comment
 from .forms import CaptchaCommentForm
 from django.http import HttpResponse
+from django.core.paginator import Paginator
 
 def comment_list(request):
-    sort_by = request.GET.get('sort_by', 'created_at')
-    order = request.GET.get('order', 'desc')
+    sort_by = request.GET.get('sort_by', 'created_at')  # По умолчанию сортируем по дате добавления
+    order = request.GET.get('order', 'desc')  # По умолчанию сортируем по убыванию
+
+    valid_sort_fields = ['username', 'email', 'created_at']
+
+    if sort_by not in valid_sort_fields:
+        sort_by = 'created_at'
 
     if order == 'asc':
         comments = Comment.objects.all().order_by(sort_by)
     else:
         comments = Comment.objects.all().order_by(f'-{sort_by}')
 
-    form = CaptchaCommentForm()
-    return render(request, 'comments/comment_list.html', {'comments': comments, 'form': form, 'sort_by': sort_by, 'order': order})
+    paginator = Paginator(comments, 25)  # 25 комментариев на страницу
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
+    form = CaptchaCommentForm()
+    return render(request, 'comments/comment_list.html', {
+        'page_obj': page_obj,
+        'form': form,
+        'sort_by': sort_by,
+        'order': order
+    })
 def add_comment(request):
     if request.method == 'POST':
         form = CaptchaCommentForm(request.POST)
