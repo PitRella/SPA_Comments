4d6f5e0974b630192ccbe7d65fe9012120f15f18
pit-rella <serezha.sliva@gmail.com>
2024-08-04 15:28:46 +0300

    Changed list of installed apps

--- SPA_Comments/settings.py
+++ SPA_Comments/settings.py
@@ -35,8 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'captcha',
     'comments',
+    'captcha',
 ]
 
 MIDDLEWARE = [
